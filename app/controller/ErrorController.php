4ea284401db270a22715721d76b93cc625209c94
Bhalexx <minia-ture@hotmail.com>
2017-08-30 19:36:27 +0200

    Comments + cleaning code

--- app/controller/ErrorController.php
+++ app/controller/ErrorController.php
@@ -4,10 +4,16 @@
 	class ErrorController extends AppController {
 		private $folder = 'error/';
 
+		/*
+		 * Renders 404 view
+		 */
 		public function notFound() {
 			$this->render($this->folder.'404.twig.html');
 		}
 
+		/*
+		 * Renders 403 view
+		 */
 		public function forbidden() {
 			$this->render($this->folder.'403.twig.html');
 		}
